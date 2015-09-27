240d30a50d6466be97f2ca356053875ff9d203e6
jenscobie <jenscobie@gmail.com>
2015-09-27 09:17:28 -0700

    Test for uninstalling package

--- spec/classes/vault_spec.rb
+++ spec/classes/vault_spec.rb
@@ -3,13 +3,23 @@ require 'spec_helper'
 describe 'vault' do
   let(:facts) { default_test_facts }
 
-  it do
-    should include_class('brewcask')
+  context 'ensure => present' do
+    let(:params) { { :ensure => 'present' } }
 
-    should contain_package('vault').with({
-      :ensure          => 'present',
-      :provider        => 'brewcask',
-      :install_options => ['--appdir=/Applications', '--binarydir=/test/boxen/bin'],
-    })
+    it do
+      should include_class('brewcask')
+
+      should contain_package('vault').with({
+        :ensure          => 'present',
+        :provider        => 'brewcask',
+        :install_options => ['--appdir=/Applications', '--binarydir=/test/boxen/bin'],
+      })
+    end
+  end
+
+  context 'ensure => absent' do
+    let(:params) { { :ensure => 'absent' } }
+
+    it { should contain_package('vault').with_ensure('absent') }
   end
 end
